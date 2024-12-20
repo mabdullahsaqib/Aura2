f0cdd8a88a8aa6db5e15dcf02270813c9d928b1f
AIbdullah <mabdullahsaqib2004@gmail.com>
2024-12-20 18:28:34 +0500

    time out and voice interaction

--- src/voice_interaction.py
+++ src/voice_interaction.py
@@ -44,7 +44,7 @@ def activate_module(command):
         task_voice_interaction(command)
     elif "web" in command or "search" in command or "browse" in command:
         web_browsing_voice_interaction(command)
-    elif "note" in command:
+    elif "note" in command or "notes" in command:
         note_voice_interaction(command)
     elif "translation" in command or "translate" in command:
         translation_voice_interaction()
